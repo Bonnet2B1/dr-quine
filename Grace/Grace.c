48dafcd38426c492fa214f9d879a218ed8fba805
Elias <elias.delarbre@gmail.com>
2026-01-13 16:39:02 +0100

    Fix error handling in Grace.c for file operations

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define GRACE			int main() {FILE *grace_kid = fopen("Grace_kid.c", "w");if (!grace_kid)return (1);if (fprintf(grace_kid, SOURCE_CODE, 10, 34, 34, 34, 34, 10, 34, SOURCE_CODE, 34, 10, 34, 34, 10, 10, 10) == EOF)return (1);return (0);}
-#define SOURCE_CODE		"#include <stdio.h>%c#define GRACE			int main() {FILE *grace_kid = fopen(%cGrace_kid.c%c, %cw%c);if (!grace_kid)return (1);if (fprintf(grace_kid, SOURCE_CODE, 10, 34, 34, 34, 34, 10, 34, SOURCE_CODE, 34, 10, 34, 34, 10, 10, 10) == EOF)return (1);return (0);}%c#define SOURCE_CODE		%c%s%c%c#define THIS_EXERCISE	%cNo fun%c%c/* comment */%cGRACE%c"
+#define GRACE			int main() {FILE *grace_kid = fopen("Grace_kid.c", "w");if (!grace_kid)return (1);if (fprintf(grace_kid, SOURCE_CODE, 10, 34, 34, 34, 34, 10, 34, SOURCE_CODE, 34, 10, 34, 34, 10, 10, 10) < 0){fclose(grace_kid);return (1);}if (fclose(grace_kid) == EOF)return (1);return (0);}
+#define SOURCE_CODE		"#include <stdio.h>%c#define GRACE			int main() {FILE *grace_kid = fopen(%cGrace_kid.c%c, %cw%c);if (!grace_kid)return (1);if (fprintf(grace_kid, SOURCE_CODE, 10, 34, 34, 34, 34, 10, 34, SOURCE_CODE, 34, 10, 34, 34, 10, 10, 10) < 0){fclose(grace_kid);return (1);}if (fclose(grace_kid) == EOF)return (1);return (0);}%c#define SOURCE_CODE		%c%s%c%c#define THIS_EXERCISE	%cNo fun%c%c/* comment */%cGRACE%c"
 #define THIS_EXERCISE	"No fun"
 /* comment */
 GRACE
